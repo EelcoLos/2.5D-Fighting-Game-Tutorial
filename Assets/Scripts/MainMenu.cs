59c980b011afe79b6a97763ea93731c2d3bbc829
Eelco Los <eelcolos@hotmail.com>
2019-01-09 12:20:41 +0100

    finished video tutorial #12

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]                                     // Add audio source when attaching script
+public class MainMenu : MonoBehaviour
+{
+
+    private string[] _mainMenuButtons = new string[]                        // Creates an array of GUI Buttons for the main menu scene
+    {
+        "_onePlayer",
+        "_twoPlayer",
+        "_quit"
+    };
+
+    private MainMenuController _mainMenuController;                         // Defines naming convention for main menu controller
+    private enum MainMenuController                                         // Defines states main menu can exist in
+    {
+        MainMenuFadeIn = 0,
+        MainMenuAtIdle = 1,
+        MainMenuFadeOut = 2
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MainMenuFadeIn()
+    {
+        Debug.Log("MainMenuFadeIn");
+    }
+    private void MainMenuAtIdle()
+    {
+        Debug.Log("MainMenuAtIdle");
+    }
+    private void MainMenuFadeOut()
+    {
+        Debug.Log("MainMenuFadeOut");
+    }
+
+    private void MainMenuButtonPress()
+    {
+        Debug.Log("MainMenuButtonPress");
+    }
+}
